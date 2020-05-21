becb625ea99ed8cc99873597d75af1ecf4fd421e
William <william.plante-tremblay@umontreal.ca>
2020-05-20 23:08:51 -0400

    Completed creation of modal, it's interactivity and it's lazy loading.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,11 +3,25 @@ import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
 
-
 new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 new StickyHeader();
+let modal;
+
+document.querySelectorAll(".open-modal").forEach(element => {
+  element.addEventListener("click", evt => {
+    evt.preventDefault();
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ "./modules/Modal").then(_modal => {
+        modal = new _modal.default();
+        setTimeout(() => modal.openTheModal(), 20);
+      }).catch(e => console.log("There was an error : " + e));
+    } else {
+      modal.openTheModal();
+    }
+  })
+});
 
 if (module.hot) {
   module.hot.accept();
